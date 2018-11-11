b10fb4a7cc712e565d6f17f2e75bab70cfb92bac
Fares Fraij <fares@cs.utexas.edu>
2018-11-11 10:48:35 -0600

    uploading additional material

---
+++ examples/ShowDatabases.sql
@@ -0,0 +1,28 @@
+-- -----------------
+-- ShowDatabases.sql
+-- -----------------
+
+-- https://www.postgresql.org/
+
+-- list available databases
+\list
+-- quit postgres
+\quit
+
+/*
+% psql -Version
+psql (PostgreSQL) 9.5.7
+%
+
+
+
+% psql -U postgres --quiet
+Password for user postgres:
+postgres=# \quit
+%
+
+
+% psql -U postgres --quiet -H -f ShowDatabases.sql > ShowDatabases.html
+Password for user postgres:
+% 
+*/
